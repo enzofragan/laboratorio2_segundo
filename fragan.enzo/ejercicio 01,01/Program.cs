ce804bad28d96294193dc77a0e4ec01114ab7159
enzo <36193661+enzofragan@users.noreply.github.com>
2019-08-20 12:06:10 -0300

    clase 1 bienvenida y conseptos basicos

---
+++ fragan.enzo/ejercicio 01,01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ejercicio 01";
+            int numero;
+            int max = 0;
+            int min = 0;
+            float promedio = 0;
+            int i;
+
+
+            for (i=0;i<5;i++)
+            {
+                Console.Write("ingrese un numero: ");
+
+                numero = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("el numero es {0}", numero);
+
+                if(max==0 && min==0)
+                {
+                    max = numero;
+                    min = numero;
+                }
+
+                if(max<numero)
+                {
+                    max = numero;
+                }
+                else if(min>numero)
+                {
+                    min = numero;
+                }
+
+                promedio = promedio + numero;
+            }
+
+            promedio = (Single) promedio / i;
+
+            Console.WriteLine("\nel valor maximo es {0}\nel valor minimo es {1}\nel promedio es {2}", max, min, promedio);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ fragan.enzo/ejercicio 01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apellido;
+            Console.Title = "ejercicio de bienvenida";
+            // Console.WriteLine("hola mundo");
+            System.Console.WriteLine("ingrese su nombre: ");
+            apellido = Console.ReadLine();
+            Console.Write("mi apellido es {0}",apellido);
+            Console.ReadLine();
+        }
+    }
+}
